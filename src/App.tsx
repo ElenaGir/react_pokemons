edd6d4188ecbeeb03667aeccb3976b50b444a5fc
ElenaGir <elena.girard@gmail.com>
2022-12-09 15:04:59 +0100

    add css

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,16 @@ const App: FunctionComponent = () => {
         <div className='container'>
             <div className='row'>
                 {pokemons.map(({id, name, picture, types}) => (
-                    <div  className='col s6 m4' key={id}>
-                        <div className='card horizontal'>
-                            <div className='card-image'>
-                                <img src={picture} alt={name} />
+                    <div  className='col s12 m6 l4' key={id}>
+                        <div className='card horizontal hoverable'>
+                            <div className='card-image '>
+                                <img  src={picture} alt={name} />
                             </div>
                             <div className='card-stacked'>
-                                <div className='card-content'>
-                                    <p>Name:{name}</p> 
-                                    <p>Type:{types}</p>
+                                <div className='card-content '>
+                                    <p className='flow-text'>{name}</p> 
+                                    <p>{types[0]}</p>
+                                    <p>{types[1]}</p>
                                 </div>
                             </div>
                         </div>
